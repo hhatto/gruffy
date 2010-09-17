f08df9bd7818b6cd974f64e18f1d34241b57efec
Hideo Hattori <hhatto.jp@gmail.com>
2010-09-18 01:10:53 +0900

    release ver0.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ sys.path.insert(0, 'gruffy')
 
 setup(
     name='gruffy',
-    version='0.0.2',
+    version='0.0.3',
     description="Gruffy is Python implemetation of Gruff(Ruby Graphing Library)",
     long_description=open("README.rst").read(),
     license='MIT',
