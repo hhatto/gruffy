400c7d6f8a6b2a4a974a44ba79df1631865e18db
Hideo Hattori <hhatto.jp@gmail.com>
2010-09-18 18:38:47 +0900

    featured: Pie Graph

--- gruffy/__init__.py
+++ gruffy/__init__.py
@@ -2,6 +2,7 @@ from gruffy.area import Area
 from gruffy.bar import Bar
 from gruffy.dot import Dot
 from gruffy.line import Line
+from gruffy.pie import Pie
 from gruffy.side_bar import SideBar
 
-__all__ = ['Area', 'Bar', 'Dot', 'Line', 'SideBar']
+__all__ = ['Area', 'Bar', 'Dot', 'Line', 'Pie', 'SideBar']
