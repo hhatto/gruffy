ce8062ba019e276657ea313001a3f14383a351f9
Hideo Hattori <hhatto.jp@gmail.com>
2010-09-18 17:25:26 +0900

    update __init__.py

--- gruffy/__init__.py
+++ gruffy/__init__.py
@@ -1,6 +1,7 @@
 from gruffy.area import Area
 from gruffy.bar import Bar
+from gruffy.dot import Dot
 from gruffy.line import Line
 from gruffy.side_bar import SideBar
 
-__all__ = ['Area', 'Bar', 'Line', 'SideBar']
+__all__ = ['Area', 'Bar', 'Dot', 'Line', 'SideBar']
