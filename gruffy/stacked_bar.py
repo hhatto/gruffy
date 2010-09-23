e0f16f2c6adfaabb5436ed3fad129dd7a40913d8
Hideo Hattori <hhatto.jp@gmail.com>
2010-09-23 19:49:19 +0900

    featured: stacked bar and side bar graph

---
+++ gruffy/stacked_bar.py
@@ -0,0 +1,48 @@
+from gruffy.base import *
+
+
+class StackedBar(Base, StackedMixin):
+    """Stacked Bar Graph Class"""
+
+    bar_spacing = None
+
+    def draw(self):
+        self.get_maximum_by_stack()
+        StackedBar.__base__.draw(self)
+        if not self.has_gdata:
+            return
+
+        # Setup spacing.
+        #
+        # Columns sit stacked.
+        self.bar_spacing = self.bar_spacing or 0.9
+        self.bar_width = self.graph_width / float(self.column_count)
+        padding = (self.bar_width * (1 - self.bar_spacing)) / 2
+
+        dl = DrawableList()
+        dl.append(DrawableStrokeOpacity(0.0))
+        height = [0 for i in range(self.column_count)]
+        for raw_index, data_row in enumerate(self.norm_data):
+            for point_index, data_point in enumerate(data_row['values']):
+                dl.append(DrawableFillColor(Color(data_row['color'])))
+                # Calculate center based on bar_width and current row
+                label_center = self.graph_left + \
+                               (self.bar_width * point_index) + \
+                               (self.bar_width * self.bar_spacing / 2.0)
+                self.draw_label(label_center, point_index)
+
+                if data_point == 0:
+                    continue
+                # Use incremented x and scaled y
+                left_x = self.graph_left + \
+                         (self.bar_width * point_index) + padding
+                left_y = self.graph_top + (self.graph_height - \
+                         data_point * self.graph_height - \
+                         height[point_index]) + 1
+                right_x = left_x + self.bar_width * self.bar_spacing
+                right_y = self.graph_top + \
+                          self.graph_height - height[point_index] - 1
+                # update the total height of the current stacked bar
+                height[point_index] += (data_point * self.graph_height)
+                dl.append(DrawableRectangle(left_x, left_y, right_x, right_y))
+        self.base_image.draw(dl)
---
+++ gruffy/stacked_sidebar.py
@@ -0,0 +1,70 @@
+from gruffy.base import *
+from gruffy.sidebar import SideBar
+
+
+class StackedSideBar(SideBar, StackedMixin):
+    """Side Stacked Bar Graph Class"""
+
+    bar_spacing = None
+
+    def draw(self):
+        self.has_left_labels = True
+        self.get_maximum_by_stack()
+        SideBar.draw(self)
+
+    def draw_bars(self):
+        # Setup spacing.
+        #
+        # Columns sit stacked.
+        self.bar_spacing = self.bar_spacing or 0.9
+
+        self.bar_width = self.graph_height / float(self.column_count)
+        dl = DrawableList()
+        dl.append(DrawableStrokeOpacity(0.0))
+        height = [0 for i in range(self.column_count)]
+        length = [self.graph_left for i in range(self.column_count)]
+        padding = (self.bar_width * (1 - self.bar_spacing)) / 2
+
+        for row_index, data_row in enumerate(self.norm_data):
+            for point_index, data_point in enumerate(data_row['values']):
+                # using the original calcs from the stacked bar chart to
+                # get the difference between part of the bart chart
+                # we wish to stack.
+                tmp1 = self.graph_left + (self.graph_width - \
+                       data_point * self.graph_width - height[point_index]) + 1
+                tmp2 = self.graph_left + \
+                       self.graph_width - height[point_index] - 1
+                difference = tmp2 - tmp1
+                dl.append(DrawableFillColor(Color(data_row['color'])))
+                if type(self.transparent) is float:
+                    dl.append(DrawableFillOpacity(self.transparent))
+                elif self.transparent is True:
+                    dl.append(DrawableFillOpacity(DEFAULT_TRANSPARENCY))
+                left_x = length[point_index]
+                left_y = self.graph_top + \
+                         (self.bar_width * point_index) + padding
+                right_x = left_x + difference
+                right_y = left_y + self.bar_width * self.bar_spacing
+                length[point_index] += difference
+                height[point_index] += (data_point * self.graph_width - 2)
+
+                dl.append(DrawableRectangle(left_x, left_y, right_x, right_y))
+
+                # Calculate center based on bar_width and current row
+                label_center = self.graph_top + \
+                               (self.bar_width * point_index) + \
+                               (self.bar_width * self.bar_spacing / 2.0)
+                self.draw_label(label_center, point_index)
+        self.base_image.draw(dl)
+
+    def larger_than_max(self, data_point, index=0):
+        if self.lmax(data_point, index) > self.maximum_value:
+            return True
+        return False
+
+    def lmax(self, data_point, index):
+        #self.gdata.inject(0) {|sum, item| sum + item['values'][index]}
+        total = 0
+        for data in self.gdata:
+            total += data['values'][index]
+        return total
