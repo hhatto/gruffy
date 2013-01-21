c96007c0f0a9fbd4df37e416a0a16c1b22218d59
Hideo Hattori <hhatto.jp@gmail.com>
2013-01-21 14:15:33 +0900

    initial commit for Bezier Graph

--- gruffy/bezier.py
+++ gruffy/bezier.py
@@ -15,21 +15,20 @@ class Bezier(Base):
         dl = DrawableList()
 
         for data_row in self.norm_data:
-            poly_points = []
-            dl.append(DrawableFillColor(data_row[DATA_COLOR_INDEX]))
+            poly_points = CoordinateList()
+            dl.append(DrawableFillColor(data_row['color']))
 
-            for data_point, index in data_row[1]:
+            for index, data_point in enumerate(data_row['values']):
                 # Use incremented x and scaled y
                 new_x = self.graph_left + (self.x_increment * index)
-                new_y = self.graph_top + (self.graph_height - data_point * self.graph_height)
+                new_y = self.graph_top + (
+                    self.graph_height - data_point * self.graph_height)
                 if index == 0:
-                    poly_points.append(self.graph_left)
-                    poly_points.append(self.graph_bottom - 1)
-                poly_points.append(new_x)
-                poly_points.append(new_y)
+                    poly_points.append(Coordinate(self.graph_left, self.graph_bottom - 1))
+                poly_points.append(Coordinate(new_x, new_y))
                 self.draw_label(new_x, index)
             dl.append(DrawableFillOpacity(0.0))
-            dl.append(DrawableStroke(data_row[DATA_COLOR_INDEX]))
-            dl.append(DrawableStrokeWidth(self.clip_value_if_greater_than(self.columns / (self.norm_data.first[1].size * 4), 5.0)))
+            dl.append(DrawableStrokeColor(data_row['color']))
+            dl.append(DrawableStrokeWidth(self.clip_value_if_greater_than(self.columns / (len(self.norm_data[0]['values']) * 4), 5.0)))
             dl.append(DrawableBezier(poly_points))
         self.base_image.draw(dl)
