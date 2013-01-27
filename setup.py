7d16cad58cfedc0afe7ce6cfea514f4296f80f0a
Hideo Hattori <hhatto.jp@gmail.com>
2013-01-27 23:46:56 +0900

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='gruffy',
-    version='0.3.3',
+    version='0.3.4',
     description="Gruffy is Python implemetation of Gruff(Ruby Graphing Library)",
     long_description=open("README.rst").read(),
     license='MIT',
