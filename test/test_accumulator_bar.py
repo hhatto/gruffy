feb7974a301de4031a65b7512fe1fc8ca79eafcc
Hideo Hattori <hhatto.jp@gmail.com>
2013-01-27 22:10:54 +0900

    first commit for accumulator_bar and bezier graph's tests

---
+++ test/test_accumulator_bar.py
@@ -0,0 +1,22 @@
+import os
+from unittest import TestCase, main
+from gruffy.accumulator_bar import AccumulatorBar
+
+TARGET_FILE = 'test.png'
+
+
+class TestAccumulatorBar(TestCase):
+
+    def tearDown(self):
+        os.remove(TARGET_FILE)
+
+    def test_writable(self):
+        g = AccumulatorBar()
+        g.theme_pastel()
+        g.theme_greyscale()
+
+        g.data("test1", [1, 2, 3])
+        g.write(TARGET_FILE)
+
+if __name__ == '__main__':
+    main()
---
+++ test/test_bezier.py
@@ -0,0 +1,23 @@
+import os
+from unittest import TestCase, main
+from gruffy.bezier import Bezier
+
+TARGET_FILE = 'test.png'
+
+
+class TestBezier(TestCase):
+
+    def tearDown(self):
+        os.remove(TARGET_FILE)
+
+    def test_writable(self):
+        g = Bezier()
+        g.theme_pastel()
+        g.theme_greyscale()
+
+        g.data("test1", [1, 2, 3])
+        g.data("test2", [3, 2, 1])
+        g.write(TARGET_FILE)
+
+if __name__ == '__main__':
+    main()
